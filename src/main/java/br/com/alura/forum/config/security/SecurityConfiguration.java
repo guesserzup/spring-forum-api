3fbb625476bd165fc5c03161762f35b62ae8d257
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-18 20:49:09 -0300

    implementing jwt

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -2,8 +2,10 @@ package br.com.alura.forum.config.security;
 
 import br.com.alura.forum.modelo.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -22,12 +24,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private AutenticacaoService autenticacaoService;
 
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
     //Configurações de autorização
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/topicos").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
---
+++ src/main/java/br/com/alura/forum/controller/dto/TokenDto.java
@@ -0,0 +1,6 @@
+package br.com.alura.forum.controller.dto;
+
+public class TokenDto {
+    public TokenDto(String token, String bearer) {
+    }
+}
