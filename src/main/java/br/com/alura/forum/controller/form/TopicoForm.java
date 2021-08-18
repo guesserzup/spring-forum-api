0d32c0412a1c62bb79d0f425f8d12406564fb4a2
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-17 21:25:23 -0300

    adding validator dependency and using its notations to validate post requests

--- src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -3,11 +3,22 @@ package br.com.alura.forum.controller.form;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
+import com.sun.istack.NotNull;
+
+import javax.validation.constraints.NotEmpty;
 
 public class TopicoForm {
 
+    @NotNull
+    @NotEmpty
     private String titulo;
+
+    @NotNull
+    @NotEmpty
     private String mensagem;
+
+    @NotNull
+    @NotEmpty
     private String nomeCurso;
 
     public String getTitulo() {
