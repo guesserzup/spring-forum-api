fd096df1deef324da7447dae99a8d5f3c41ee808
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-17 21:47:10 -0300

    creating a DTO for our error handle method

--- src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
+++ src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
@@ -1,10 +1,20 @@
 package br.com.alura.forum.config.validacao;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class ErroDeValidacaoHandler {
 
-    
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ErroDeFormularioDTO> handle(MethodArgumentNotValidException exception) {
+
+    }
 
 }
