b6b3c0d755fd67655a46c3218ef7ee1e9b27e0cd
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-18 15:23:07 -0300

    allowing access to get endpoints

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -14,7 +15,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     //Configurações de autorização
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
     }
 
     //Configurações de recursos estáticos(js, css, imagens, etc...)
