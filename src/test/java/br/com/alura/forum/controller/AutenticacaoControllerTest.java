4e2f42a1b0f931c262eb15900a558b9e27c11d8e
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-19 12:48:29 -0300

    creating a test for the controller

--- src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
+++ src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
@@ -1,13 +1,34 @@
 package br.com.alura.forum.controller;
 
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
 class AutenticacaoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnStatus400IfAuthenticationDataIsIncorrect() throws Exception {
+        URI uri = new URI("/auth");
+        String json = "{\"email\":\"invalido@email.com\", \"senha\":\"123456\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post(uri).content(json).contentType(json)).andExpect(MockMvcResultMatchers.status().is(400));
+    }
 
 }
